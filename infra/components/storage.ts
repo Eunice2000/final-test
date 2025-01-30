192bda6f312dfb86db74dfbef72487381ddb30cc
Eunice js <90851478+Eunice2000@users.noreply.github.com>
2025-01-30 16:01:32 +0100

    Update storage.ts

--- infra/components/storage.ts
+++ infra/components/storage.ts
@@ -1,3 +1,4 @@
+import * as pulumi from "@pulumi/pulumi";
 import * as gcp from "@pulumi/gcp";
 
 export function createStorageBucket(name: string) {
@@ -8,9 +9,11 @@ export function createStorageBucket(name: string) {
 }
 
 export function uploadStaticAsset(bucket: gcp.storage.Bucket, filename: string, content: string) {
-    return new gcp.storage.BucketObject(filename, {
-        bucket: bucket.name,
-        content: content,
-        contentType: "text/html",
+    return bucket.name.apply(name => {
+        return new gcp.storage.BucketObject(filename, {
+            bucket: name, // <-- Use the resolved value
+            content: content,
+            contentType: "text/html",
+        });
     });
 }
